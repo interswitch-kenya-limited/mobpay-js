938fd61014bf3fa08de98733796582eb97419be8
Allan Mageto <allanbmageto@gmail.com>
2019-08-29 17:21:19 +0300

    Project created

---
+++ index.js
@@ -0,0 +1,19 @@
+exports.popUpPaymentiFrame = function () {
+    console.log("This is a message from the demo package");
+}
+
+exports.submitCardPayment = function (){
+    console.log("submit payment card function")
+}
+
+exports.submitCardTokenPayment = function (){
+    console.log("submit token payment")
+}
+
+exports.confirmMobilePayment = function (){
+    console.log("confirm Mobile payment")
+}
+
+exports.submitMobilePayment = function (){
+    
+}
